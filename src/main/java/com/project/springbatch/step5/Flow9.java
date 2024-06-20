27af8926498b72ef4740df0a4f1b9d4eb69016cf
mugeon <pos04167@kakao.com>
2024-06-20 21:57:48 +0900

    batch study

--- src/main/java/com/project/springbatch/step5/Flow9.java
+++ src/main/java/com/project/springbatch/step5/Flow9.java
@@ -1,129 +1,129 @@
-package com.project.springbatch.step5;
-
-import com.project.springbatch.BatchConfig;
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
-
-/**
- * JobScope
- */
-@Configuration
-@RequiredArgsConstructor
-public class Flow9 {
-    private final JobRepository jobRepository;
-    private final PlatformTransactionManager transactionManager;
-
-
-    @Bean
-    public Job job(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new JobBuilder("batchJob", jobRepository)
-                .incrementer(new RunIdIncrementer())
-                .start(step1(null))
-                .build();
-    }
-
-    @Bean
-    public Step step1(@Value("#{jobParameters['message']}") String message) {
-
-        System.out.println("message >> :  " + message);
-
-        return new StepBuilder("step1", jobRepository)
-                .tasklet(tasklet1(null), transactionManager)
-                .build();
-    }
-
-    @Bean
-    @StepScope
-    public Tasklet tasklet1(@Value("#{jobParameters['name']}") String name) {
-        return (contribution, chunkContext) -> {
-            System.out.println("hello tasklet1");
-            return RepeatStatus.FINISHED;
-        };
-    }
-
-    @Bean
-    public Step step2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("step2", jobRepository)
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("start step2 hello world");
-                        return RepeatStatus.FINISHED;
-                    }
-                }, transactionManager)
-                .build();
-    }
-
-
-    @Bean
-    public Step step3(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("step3", jobRepository)
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("start step3 hello world");
-                        return RepeatStatus.FINISHED;
-                    }
-                }, transactionManager)
-                .build();
-    }
-
-
-    //
-
-    @Bean
-    public Step step4(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("step4", jobRepository)
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("start step4 hello world");
-                        return RepeatStatus.FINISHED;
-                    }
-                }, transactionManager)
-                .build();
-    }
-
-    @Bean
-    public Step step5(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("step5", jobRepository)
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("start step5 hello world");
-                        return RepeatStatus.FINISHED;
-                    }
-                }, transactionManager)
-                .build();
-    }
-
-    @Bean
-    public Step step6(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("step6", jobRepository)
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("start step6 hello world");
-                        return RepeatStatus.FINISHED;
-                    }
-                }, transactionManager)
-                .build();
-    }
-
-}
+//package com.project.springbatch.step5;
+//
+//import com.project.springbatch.BatchConfig;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.StepContribution;
+//import org.springframework.batch.core.configuration.annotation.StepScope;
+//import org.springframework.batch.core.job.builder.FlowBuilder;
+//import org.springframework.batch.core.job.builder.JobBuilder;
+//import org.springframework.batch.core.job.flow.Flow;
+//import org.springframework.batch.core.launch.support.RunIdIncrementer;
+//import org.springframework.batch.core.repository.JobRepository;
+//import org.springframework.batch.core.scope.context.ChunkContext;
+//import org.springframework.batch.core.step.builder.StepBuilder;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+///**
+// * JobScope
+// */
+//@Configuration
+//@RequiredArgsConstructor
+//public class Flow9 {
+//    private final JobRepository jobRepository;
+//    private final PlatformTransactionManager transactionManager;
+//
+//
+//    @Bean
+//    public Job job(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+//        return new JobBuilder("batchJob", jobRepository)
+//                .incrementer(new RunIdIncrementer())
+//                .start(step1(null))
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step1(@Value("#{jobParameters['message']}") String message) {
+//
+//        System.out.println("message >> :  " + message);
+//
+//        return new StepBuilder("step1", jobRepository)
+//                .tasklet(tasklet1(null), transactionManager)
+//                .build();
+//    }
+//
+//    @Bean
+//    @StepScope
+//    public Tasklet tasklet1(@Value("#{jobParameters['name']}") String name) {
+//        return (contribution, chunkContext) -> {
+//            System.out.println("hello tasklet1");
+//            return RepeatStatus.FINISHED;
+//        };
+//    }
+//
+//    @Bean
+//    public Step step2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+//        return new StepBuilder("step2", jobRepository)
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println("start step2 hello world");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                }, transactionManager)
+//                .build();
+//    }
+//
+//
+//    @Bean
+//    public Step step3(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+//        return new StepBuilder("step3", jobRepository)
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println("start step3 hello world");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                }, transactionManager)
+//                .build();
+//    }
+//
+//
+//    //
+//
+//    @Bean
+//    public Step step4(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+//        return new StepBuilder("step4", jobRepository)
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println("start step4 hello world");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                }, transactionManager)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step5(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+//        return new StepBuilder("step5", jobRepository)
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println("start step5 hello world");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                }, transactionManager)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step6(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+//        return new StepBuilder("step6", jobRepository)
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println("start step6 hello world");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                }, transactionManager)
+//                .build();
+//    }
+//
+//}
---
+++ src/main/java/com/project/springbatch/step6/Chunk1.java
@@ -0,0 +1,85 @@
+package com.project.springbatch.step6;
+
+import com.project.springbatch.BatchConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * Chunk
+ */
+@Configuration
+@RequiredArgsConstructor
+public class Chunk1 {
+
+    private final BatchConfig batchConfig;
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    private static final Integer CHUNK_SIZE = 2;
+
+    @Bean
+    public Job job(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new JobBuilder("batchJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return new StepBuilder("step1", jobRepository)
+                .<String, String>chunk(CHUNK_SIZE, transactionManager) // SimpleStepBuilder > TaskletStep > ChunkOrientedTasklet 생성
+                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
+
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        return "my_"+ item;
+                    }
+                })
+
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(Chunk<? extends String> chunk) throws Exception {
+                        List<? extends String> items = chunk.getItems();
+                        items.stream()
+                                .forEach(System.out::println);
+                    }
+                })
+
+                .build();
+    }
+    @Bean
+    public Step step2() {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("start step2 hello world");
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager)
+                .build();
+    }
+}
