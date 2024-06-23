afe70302bcc1186b85f251fb7217c93e4b8b6ab4
mugeon <pos04167@kakao.com>
2024-06-23 12:17:18 +0900

    batch study

--- src/main/java/com/project/springbatch/step6/Chunk3.java
+++ src/main/java/com/project/springbatch/step6/Chunk3.java
@@ -4,23 +4,16 @@ import com.project.springbatch.BatchConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.item.Chunk;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.support.ListItemReader;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -37,12 +30,15 @@ public class Chunk3 {
 
     private static final Integer CHUNK_SIZE = 2;
 
+    /**
+     *  처음에 0~6에서 chunk size가 2 이기 때문에
+     *  {0,1} , {2,3} , {4,5} ,{6}
+     *
+     */
     @Bean
     public Job job(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new JobBuilder("batchJob", jobRepository)
-                .incrementer(new RunIdIncrementer())
                 .start(step1())
-                .next(step2())
                 .build();
     }
 
@@ -50,36 +46,101 @@ public class Chunk3 {
     public Step step1() {
         return new StepBuilder("step1", jobRepository)
                 .<String, String>chunk(CHUNK_SIZE, transactionManager) // SimpleStepBuilder > TaskletStep > ChunkOrientedTasklet 생성
-                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
-
-                .processor(new ItemProcessor<String, String>() {
-                    @Override
-                    public String process(String item) throws Exception {
-                        return "my_"+ item;
-                    }
-                })
-
-                .writer(new ItemWriter<String>() {
-                    @Override
-                    public void write(Chunk<? extends String> chunk) throws Exception {
-                        List<? extends String> items = chunk.getItems();
-                        items.stream()
-                                .forEach(System.out::println);
-                    }
-                })
-
+                .reader(itemReader())
+                .writer(itemWriter())
                 .build();
     }
-    @Bean
-    public Step step2() {
-        return new StepBuilder("step2", jobRepository)
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("start step2 hello world");
-                        return RepeatStatus.FINISHED;
-                    }
-                }, transactionManager)
-                .build();
+
+    private ItemWriter<? super String> itemWriter() {
+
+        return new CustomItemWriter();
+
+    }
+
+
+    private CustomItemStreamReader itemReader() {
+        List<String> items = new ArrayList<>(10);
+
+        for (int i = 0; i < 10; i++) {
+            items.add(String.valueOf(i));
+        }
+        return new CustomItemStreamReader(items);
+    }
+
+
+    class CustomItemStreamReader implements ItemStreamReader<String> {
+
+        private final List<String> items;
+        private int index = -1;
+        private boolean restart = false;
+
+        public CustomItemStreamReader(List<String> items) {
+            this.items = items;
+            this.index = 0;
+        }
+
+        @Override
+        public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+            String item = null;
+
+            if (this.index < this.items.size()) {
+                item = this.items.get(index);
+                index++;
+            }
+
+            if (this.index == 6 && !restart) throw new RuntimeException("Restart is required");
+            return item;
+        }
+
+
+        @Override
+        public void open(ExecutionContext executionContext) throws ItemStreamException {
+            if (executionContext.containsKey("index")) {
+                index = executionContext.getInt("index");
+                this.restart = true;
+            } else {
+                System.out.println("초기화");
+                index = 0;
+                executionContext.put("index", index);
+            }
+        }
+
+        @Override
+        public void update(ExecutionContext executionContext) throws ItemStreamException {
+            executionContext.put("index", index);
+        }
+
+        /**
+         * 예외가 발생하면 실행
+         */
+        @Override
+        public void close() throws ItemStreamException {
+            System.out.println(" ==== close ==== ");
+        }
+    }
+
+
+    class CustomItemWriter implements ItemStreamWriter<String> {
+
+        @Override
+        public void write(Chunk<? extends String> chunk) throws Exception {
+            chunk.getItems().forEach(System.out::println);
+
+        }
+
+        @Override
+        public void open(ExecutionContext executionContext) throws ItemStreamException {
+            System.out.println("writer open");
+        }
+
+        @Override
+        public void update(ExecutionContext executionContext) throws ItemStreamException {
+            System.out.println("writer update");
+        }
+
+        @Override
+        public void close() throws ItemStreamException {
+            System.out.println("writer close");
+        }
     }
 }
