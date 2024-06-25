9667a9e55071cf6a5ab9da17c445413e59aee764
mugeon <pos04167@kakao.com>
2024-06-25 20:44:47 +0900

    batch study

--- src/main/java/com/project/springbatch/step6/Chunk3.java
+++ src/main/java/com/project/springbatch/step6/Chunk3.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.*;
